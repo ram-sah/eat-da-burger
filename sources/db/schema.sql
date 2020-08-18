0ed74ea3bb23f8b241bb60f92927fd5b530108bb
Ram Sah <mohan2036@gmail.com>
2020-08-17 23:39:52 -0400

    created burgers_db database

---
+++ sources/db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+use burgers_db;
+
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN DEFAULT false
+    )
