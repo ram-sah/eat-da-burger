c25189010ba9a3e51abf6f8174cd74dcd5a9c1d3
Ram Sah <mohan2036@gmail.com>
2020-08-17 23:41:01 -0400

    added values of database

---
+++ sources/db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES
+	("Quinoa Veggie Burger"),
+    ("Black Bean Veggie Burger"),
+    ("Mushroom Cap Veggie Burger"),
+    ("Wild Rice and White Mushrooms Burger");
+    SELECT * FROM burgers;
