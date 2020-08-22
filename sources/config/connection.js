a06f387b402db04e055f333a180b85da96ef4a31
Ram Sah <ram@Rams-MacBook-Pro.local>
2020-08-21 23:38:30 -0400

    add code for connection

--- sources/config/connection.js
+++ sources/config/connection.js
@@ -13,3 +13,15 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
+
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
