c4100a69cc1c1d8039b52487adf697d597fbb4d5
Ram Sah <ram@Rams-MacBook-Pro.local>
2020-08-21 23:44:00 -0400

    add code for post , put router

--- sources/controllers/burgers_controller.js
+++ sources/controllers/burgers_controller.js
@@ -1,13 +1,64 @@
 // Packages
 const express = require('express');
+
 // Local dependencies
 const burger = require('../models/burger.js');
 
+// Create the routers
+const router = express.Router();
 
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
-
-
-
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+      burger.delete(condition, function (result) {
+          console.log('In delete');
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 
 // Export routes for server.js to use.
 module.exports = router;
