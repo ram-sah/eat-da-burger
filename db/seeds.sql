4a79a64eb0688264ec6f72d7535a25aee87e650c
Ram Sah <mohan2036@gmail.com>
2020-08-24 21:27:53 -0400

    modified database value

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,6 +3,6 @@ use burgers_db;
 INSERT INTO burgers (burger_name) VALUES
 	("Veggie Burger"),
     ("Cheese Burger"),
-    ("Mushroom Cap Veggie Burger"),
+    ("Mushroom Burger"),
     ("Ham Burger");
     SELECT * FROM burgers;
